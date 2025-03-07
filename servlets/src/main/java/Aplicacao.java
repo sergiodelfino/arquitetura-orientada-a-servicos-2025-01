0c7d0e292494103c275c6b5ac9fc1b7ef1ec736d
sergiodelfino <sergio.delfino@unifio.edu.br>
2025-03-07 20:05:10 -0300

    Aula do dia 07/03/2025

--- servlets/src/main/java/Aplicacao.java
+++ servlets/src/main/java/Aplicacao.java
@@ -1,6 +1,13 @@
-import org.eclipse.jetty.ee10.servlet.ServletContextHandler;
-import org.eclipse.jetty.ee10.servlet.ServletHolder;
+import org.eclipse.jetty.security.ConstraintMapping;
+import org.eclipse.jetty.security.ConstraintSecurityHandler;
+import org.eclipse.jetty.security.HashLoginService;
+import org.eclipse.jetty.security.UserStore;
+import org.eclipse.jetty.security.authentication.BasicAuthenticator;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.security.Constraint;
+import org.eclipse.jetty.util.security.Credential;
 
 public class Aplicacao {
     public static void main(String[] args) throws Exception {
@@ -9,8 +16,30 @@ public class Aplicacao {
         ServletContextHandler contexto = new ServletContextHandler(ServletContextHandler.SESSIONS);
         contexto.setContextPath("/api");
         contexto.addServlet(new ServletHolder(new Calculadora()), "/calculadora/*");
-
         servidor.setHandler(contexto);
+
+        Constraint restricao = new Constraint();
+        restricao.setName(Constraint.__BASIC_AUTH);
+        restricao.setAuthenticate(true);
+        restricao.setRoles(new String[]{"administrador", "usuario"});
+
+        ConstraintMapping mapeamento = new ConstraintMapping();
+        mapeamento.setConstraint(restricao);
+        mapeamento.setPathSpec("/calculadora/*");
+
+        UserStore usuarios = new UserStore();
+        usuarios.addUser("sergio", Credential.getCredential("q1w2e3r4"), new String[]{"administrador", "usuario"});
+        usuarios.addUser("patricia", Credential.getCredential("toor"), new String[]{"usuario"});
+
+        HashLoginService autenticacao = new HashLoginService();
+        autenticacao.setUserStore(usuarios);
+
+        ConstraintSecurityHandler seguranca = new ConstraintSecurityHandler();
+        seguranca.addConstraintMapping(mapeamento);
+        seguranca.setLoginService(autenticacao);
+        seguranca.setAuthenticator(new BasicAuthenticator());
+
+        contexto.setSecurityHandler(seguranca);
 
         servidor.start();
         servidor.join();
